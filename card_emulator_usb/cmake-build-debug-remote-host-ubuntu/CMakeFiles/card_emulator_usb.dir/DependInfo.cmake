4437cce561bcf29d7fcd7075d7a1a49b4318440b
matthew5025 <matthew5025@users.noreply.github.com>
2021-07-14 15:39:40 +0800

    Adds alternate library

---
+++ card_emulator_usb/cmake-build-debug-remote-host-ubuntu/CMakeFiles/card_emulator_usb.dir/DependInfo.cmake
@@ -0,0 +1,56 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "C"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_C
+  "/tmp/tmp.0l2CKXr8AY/Application/main.c" "/tmp/tmp.0l2CKXr8AY/cmake-build-debug-remote-host-ubuntu/CMakeFiles/card_emulator_usb.dir/Application/main.c.o"
+  "/tmp/tmp.0l2CKXr8AY/NfcLibrary/NdefLibrary/src/P2P_NDEF.c" "/tmp/tmp.0l2CKXr8AY/cmake-build-debug-remote-host-ubuntu/CMakeFiles/card_emulator_usb.dir/NfcLibrary/NdefLibrary/src/P2P_NDEF.c.o"
+  "/tmp/tmp.0l2CKXr8AY/NfcLibrary/NdefLibrary/src/RW_NDEF.c" "/tmp/tmp.0l2CKXr8AY/cmake-build-debug-remote-host-ubuntu/CMakeFiles/card_emulator_usb.dir/NfcLibrary/NdefLibrary/src/RW_NDEF.c.o"
+  "/tmp/tmp.0l2CKXr8AY/NfcLibrary/NdefLibrary/src/RW_NDEF_MIFARE.c" "/tmp/tmp.0l2CKXr8AY/cmake-build-debug-remote-host-ubuntu/CMakeFiles/card_emulator_usb.dir/NfcLibrary/NdefLibrary/src/RW_NDEF_MIFARE.c.o"
+  "/tmp/tmp.0l2CKXr8AY/NfcLibrary/NdefLibrary/src/RW_NDEF_T1T.c" "/tmp/tmp.0l2CKXr8AY/cmake-build-debug-remote-host-ubuntu/CMakeFiles/card_emulator_usb.dir/NfcLibrary/NdefLibrary/src/RW_NDEF_T1T.c.o"
+  "/tmp/tmp.0l2CKXr8AY/NfcLibrary/NdefLibrary/src/RW_NDEF_T2T.c" "/tmp/tmp.0l2CKXr8AY/cmake-build-debug-remote-host-ubuntu/CMakeFiles/card_emulator_usb.dir/NfcLibrary/NdefLibrary/src/RW_NDEF_T2T.c.o"
+  "/tmp/tmp.0l2CKXr8AY/NfcLibrary/NdefLibrary/src/RW_NDEF_T3T.c" "/tmp/tmp.0l2CKXr8AY/cmake-build-debug-remote-host-ubuntu/CMakeFiles/card_emulator_usb.dir/NfcLibrary/NdefLibrary/src/RW_NDEF_T3T.c.o"
+  "/tmp/tmp.0l2CKXr8AY/NfcLibrary/NdefLibrary/src/RW_NDEF_T4T.c" "/tmp/tmp.0l2CKXr8AY/cmake-build-debug-remote-host-ubuntu/CMakeFiles/card_emulator_usb.dir/NfcLibrary/NdefLibrary/src/RW_NDEF_T4T.c.o"
+  "/tmp/tmp.0l2CKXr8AY/NfcLibrary/NdefLibrary/src/RW_NDEF_T5T.c" "/tmp/tmp.0l2CKXr8AY/cmake-build-debug-remote-host-ubuntu/CMakeFiles/card_emulator_usb.dir/NfcLibrary/NdefLibrary/src/RW_NDEF_T5T.c.o"
+  "/tmp/tmp.0l2CKXr8AY/NfcLibrary/NdefLibrary/src/T4T_NDEF_emu.c" "/tmp/tmp.0l2CKXr8AY/cmake-build-debug-remote-host-ubuntu/CMakeFiles/card_emulator_usb.dir/NfcLibrary/NdefLibrary/src/T4T_NDEF_emu.c.o"
+  "/tmp/tmp.0l2CKXr8AY/NfcLibrary/NxpNci/src/NxpNci.c" "/tmp/tmp.0l2CKXr8AY/cmake-build-debug-remote-host-ubuntu/CMakeFiles/card_emulator_usb.dir/NfcLibrary/NxpNci/src/NxpNci.c.o"
+  "/tmp/tmp.0l2CKXr8AY/NfcTask/src/ndef_helper.c" "/tmp/tmp.0l2CKXr8AY/cmake-build-debug-remote-host-ubuntu/CMakeFiles/card_emulator_usb.dir/NfcTask/src/ndef_helper.c.o"
+  "/tmp/tmp.0l2CKXr8AY/NfcTask/src/nfc_task.c" "/tmp/tmp.0l2CKXr8AY/cmake-build-debug-remote-host-ubuntu/CMakeFiles/card_emulator_usb.dir/NfcTask/src/nfc_task.c.o"
+  "/tmp/tmp.0l2CKXr8AY/TML/src/framework_Allocator.c" "/tmp/tmp.0l2CKXr8AY/cmake-build-debug-remote-host-ubuntu/CMakeFiles/card_emulator_usb.dir/TML/src/framework_Allocator.c.o"
+  "/tmp/tmp.0l2CKXr8AY/TML/src/framework_Container.c" "/tmp/tmp.0l2CKXr8AY/cmake-build-debug-remote-host-ubuntu/CMakeFiles/card_emulator_usb.dir/TML/src/framework_Container.c.o"
+  "/tmp/tmp.0l2CKXr8AY/TML/src/framework_Map.c" "/tmp/tmp.0l2CKXr8AY/cmake-build-debug-remote-host-ubuntu/CMakeFiles/card_emulator_usb.dir/TML/src/framework_Map.c.o"
+  "/tmp/tmp.0l2CKXr8AY/TML/src/framework_Parcel.c" "/tmp/tmp.0l2CKXr8AY/cmake-build-debug-remote-host-ubuntu/CMakeFiles/card_emulator_usb.dir/TML/src/framework_Parcel.c.o"
+  "/tmp/tmp.0l2CKXr8AY/TML/src/framework_Timer.c" "/tmp/tmp.0l2CKXr8AY/cmake-build-debug-remote-host-ubuntu/CMakeFiles/card_emulator_usb.dir/TML/src/framework_Timer.c.o"
+  "/tmp/tmp.0l2CKXr8AY/TML/src/framework_log.c" "/tmp/tmp.0l2CKXr8AY/cmake-build-debug-remote-host-ubuntu/CMakeFiles/card_emulator_usb.dir/TML/src/framework_log.c.o"
+  "/tmp/tmp.0l2CKXr8AY/TML/src/framework_mutex.c" "/tmp/tmp.0l2CKXr8AY/cmake-build-debug-remote-host-ubuntu/CMakeFiles/card_emulator_usb.dir/TML/src/framework_mutex.c.o"
+  "/tmp/tmp.0l2CKXr8AY/TML/src/framework_sem.c" "/tmp/tmp.0l2CKXr8AY/cmake-build-debug-remote-host-ubuntu/CMakeFiles/card_emulator_usb.dir/TML/src/framework_sem.c.o"
+  "/tmp/tmp.0l2CKXr8AY/TML/src/framework_thread.c" "/tmp/tmp.0l2CKXr8AY/cmake-build-debug-remote-host-ubuntu/CMakeFiles/card_emulator_usb.dir/TML/src/framework_thread.c.o"
+  "/tmp/tmp.0l2CKXr8AY/TML/src/hid.c" "/tmp/tmp.0l2CKXr8AY/cmake-build-debug-remote-host-ubuntu/CMakeFiles/card_emulator_usb.dir/TML/src/hid.c.o"
+  "/tmp/tmp.0l2CKXr8AY/TML/src/lpcusbsio.c" "/tmp/tmp.0l2CKXr8AY/cmake-build-debug-remote-host-ubuntu/CMakeFiles/card_emulator_usb.dir/TML/src/lpcusbsio.c.o"
+  "/tmp/tmp.0l2CKXr8AY/TML/src/tml.c" "/tmp/tmp.0l2CKXr8AY/cmake-build-debug-remote-host-ubuntu/CMakeFiles/card_emulator_usb.dir/TML/src/tml.c.o"
+  "/tmp/tmp.0l2CKXr8AY/TML/src/tml_hid.c" "/tmp/tmp.0l2CKXr8AY/cmake-build-debug-remote-host-ubuntu/CMakeFiles/card_emulator_usb.dir/TML/src/tml_hid.c.o"
+  )
+set(CMAKE_C_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_C
+  "CARDEMU_RAW_EXCHANGE"
+  "CARDEMU_SUPPORT"
+  )
+
+# The include file search paths:
+set(CMAKE_C_TARGET_INCLUDE_PATH
+  "../TML/inc"
+  "../NfcLibrary/inc"
+  "../NfcLibrary/NxpNci/inc"
+  "../NfcLibrary/NdefLibrary/inc"
+  "../NfcTask/inc"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
